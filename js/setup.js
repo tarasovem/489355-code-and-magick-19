6389986a7ba385c1f0425cb560cc218e5f2268f9
Evgeniy Tarasov <tarasovem@gmail.com>
2020-02-21 00:40:08 +0300

    Добавляет валидацию имени персонажа

--- js/setup.js
+++ js/setup.js
@@ -101,3 +101,13 @@ userNameInput.addEventListener('keydown', function (evt) {
     evt.stopPropagation();
   }
 });
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя персонажа не может содержать менее 2 символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Максимальная длина имени персонажа — 25 символов');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
